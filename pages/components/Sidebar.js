801b9098e150a40f61de91725f972921cd7fc3a0
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-04-27 12:40:45 -0500

    added mui icons

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -1,11 +1,37 @@
+import styled from "styled-components";
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
 function Sidebar() {
     return (
         <Container>
-            
+            <Header>
+               <UserAvatar/>
+            <IconsContainer>
+                <IconButton>
+                <ChatIcon/>
+                </IconButton>
+                <IconButton>
+                <MoreVertIcon/>
+                </IconButton>
+                
+            </IconsContainer>
+            </Header>
         </Container>
     )
 }
 
 export default Sidebar;
 
-const Container = `styled.div`
+const IconsContainer = styled.div``;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)`
+
+
+`;
