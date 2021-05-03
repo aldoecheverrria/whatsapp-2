b1daf4de633b60135187fe67681bd44f42b5d32d
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-03 11:50:18 -0500

    updating with loading js, and some updates in app

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,28 @@
 import Head  from 'next/head';
 import styled from 'styled-components';
-
+import { Button } from '@material-ui/core';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, provider } from "../firebase"
 
 function Login() {
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).catch(alert);
+    }
+useAuthState
     return (
         <Container>
             <Head>
                 <title>Entrar</title>
+             
             </Head>
-        <LoginContainer>
-            <Logo/>
-        </LoginContainer>
+            <LoginContainer>
+                <Logo src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"/>
+                <Button 
+                    variant="outlined"
+                    onClick={signIn}
+                    >¡Entra con tu cuenta Google!</Button>
+            </LoginContainer>
         </Container>
 
         
@@ -19,8 +31,25 @@ function Login() {
 
 export default Login;
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background-color: whitesmoke;
+`;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+    padding: 100px;
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0px 4px 14px -3px rgba(0, 0, 0,0.7)
+`;
 
-const Logo = styled.div``;
+const Logo = styled.img`
+    height: 200px;
+    width: 200px;
+    margin-bottom: 50px;
+`;
