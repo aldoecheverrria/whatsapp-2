c8da00b2ab9f8dfdf5277502100d451e4d21194b
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-03 20:00:15 -0500

    now loading function correctly

--- components/Loading.js
+++ components/Loading.js
@@ -1,20 +1,20 @@
-
-import { Circle } from "better-react-spinkit"
+import { Circle } from "better-react-spinkit";
 
 function Loading() {
-    return (
-        <center style= {{display: "grid", placeItems: "center", heigh: "100vh"}}>
-            <div>
-                <img
-                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
-                    alt="Whatsapp Logo"
-                    style= {{ marginBottom: 10 }}
-                    height={200}
-                />
-                <Circle color="#3CBC28" size={60} />
-            </div>
-        </center>
-    )
+  return (
+    <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
+      <div>
+        <img
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt=""
+          style={{ marginBottom: 10 }}
+          height={200}
+        />
+        <Circle color="#3CBC28" size={60} />
+      </div>
+    </center>
+  );
 }
 
-export default Loading
+export default Loading;
+
