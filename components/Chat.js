25d37dd2b2ae6fb056502ad6ad8d42c1c6064560
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-05-03 22:09:08 -0500

    fail safe and checkout another git

---
+++ components/Chat.js
@@ -0,0 +1,11 @@
+function Chat({id, users}) {
+    return (
+        <Container>
+        <p>Recepient Email</p>
+        </Container>
+    )
+}
+
+export default Chat
+
+const Container = styled.Container``;
